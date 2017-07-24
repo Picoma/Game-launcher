15135e77d47a88ef4cca229df7dfbd777c07534c
Picoma <Picoma@users.noreply.github.com>
2017-07-24 13:03:36 +0200

    Added arbitrary shortcut support + begin wine prefix support

--- prototype.py
+++ prototype.py
@@ -10,69 +10,72 @@ import subprocess, os, fnmatch
 Notify.init("Game Launcher")
 
 def find(path, file):
-    result = []
-    for root, dirs, files in os.walk(path):
-        for name in files:
-            if fnmatch.fnmatch(name, file):
-                result.append(os.path.join(root, name))
-    return result
+	result = []
+	for root, dirs, files in os.walk(path):
+		for name in files:
+			if fnmatch.fnmatch(name, file):
+				result.append(os.path.join(root, name))
+	return result
 
-def read_config(config_path="./config"):
-	configFile=open(config_path,"r")
-	config=configFile.readlines()
-	configFile.close()
-	
-	gogIndex=0
-	for index,path in enumerate(config) :
-		if "GOG folder" in path :
-			gogIndex=index
-	gogPath=config[gogIndex].split(":")[1]
-	if gogPath[-1]=="\n":
-		gogPath=gogPath[:-1]
-	return(gogPath)
-
-def on_appButton_clicked(widget):
-	FlowBoxChild,FlowBoxParent = widget.props.parent, widget.props.parent.props.parent
+def on_appButton_clicked(button):
+	FlowBoxChild,FlowBoxParent = button.props.parent, button.props.parent.props.parent
 	index=FlowBoxChild.get_index()
-	FlowBoxParent.select_child(widget.props.parent)
-	# Updating description
-	gameinfoFile=open(gamePaths[index]+"/gameinfo","r")
-	gameinfo=gameinfoFile.read()
-	gameinfoFile.close()
-	description.set_text(gameinfo)
+	FlowBoxParent.select_child(FlowBoxChild)
 
-def create_lib(flowbox):
-	gogPath=read_config()
-	proc=subprocess.Popen(['ls',gogPath],stdout=subprocess.PIPE)
-	liste=str(proc.stdout.read())[2:-1].split("\\n")[:-1] # "b'day-mode.sh\nnight-mode.sh\nprototype.py\n'" -> ['day-mode.sh', 'night-mode.sh', 'prototype.py']
+def create_lib(flowbox,gameFolderRoot):
+	try :
+		gameListe=os.listdir(gameFolderRoot)
+	except FileNotFoundError :
+		return(None)
 	global gamePaths
 	gamePaths={}
-	for index,pathname in enumerate(liste) :
-		path=gogPath+pathname
+	for index,gameName in enumerate(gameListe) :
+		path=gameFolderRoot+gameName
 		gamePaths[index]=path
 		
-		#Generate the application button
-		button=Gtk.Button(label=pathname)
-		button.connect("clicked",on_appButton_clicked)
-		button.set_image(Gtk.Image.new_from_file(path+"/game/icon.png"))
-		button.set_image_position(Gtk.PositionType.TOP)
-		flowbox.add(button) # ... and adds it to the flowbox
+		#Generate the application "button"
+		box=Gtk.Box(orientation=1)
+		icon=Gtk.Image.new_from_file(find(path, "icon*")[0])
+		title=Gtk.Label(gameName)
+		box.add(icon)
+		box.add(title)
+		flowbox.add(box)
 
 class Handler:
-	def onDeleteWindow(self, *args):
+	def onDeleteMainWindow(self, *args):
 		Gtk.main_quit(*args)
+	
+	def onDeletenewShortcutWindow(self, *args):
+		newShortcutWindow.hide()
+		newShortcutWindow.unrealize()
+		return True
 
 	def on_launch_click(self, button):
+		# Get the launch script
 		selectedChild=flowbox.get_selected_children()[0]
 		requestedPath=gamePaths[selectedChild.get_index()]
-		
-		subprocess.Popen([requestedPath+"/start.sh"]) #The starting script always has the same name, it seems
-		
+		# Creates the environment
+		gameEnv=os.environ.copy()
+		if GALLIUM_fps.get_active() and GALLIUM_cpu.get_active():
+			gameEnv["GALLIUM_HUD"]="fps,cpu"
+		else:
+			if GALLIUM_cpu.get_active():
+				gameEnv["GALLIUM_HUD"]="cpu"
+			if GALLIUM_fps.get_active():
+				gameEnv["GALLIUM_HUD"]="fps"
+			# That part is not really clever, but it works since there's only 2 elements ; maybe i'll rework that if i want to add more arguments
+		if disable_compositor.get_active():
+			subprocess.Popen(["killall","compton"]) # There is no way to automatically detect the compositor ; replace this command with your own to disable it !
+		if WINEDEBUG_Entry.get_text()!="":
+			gameEnv["WINEDEBUG"]=WINEDEBUG_Entry.get_text()
+		# Creates the processus
+		subprocess.Popen([requestedPath+"/start.sh"],env=gameEnv)
+		# Creates the notifiation
 		GameName=requestedPath.split("/")[-1]
 		iconPath=find(requestedPath,"*icon*")[0]
 		launched_notif=Notify.Notification.new("Game launched :", GameName, iconPath)
 		launched_notif.show()
-	
+
 	def on_uninstall_click(self, button):
 		# First run the confirmation dialog
 		confirmDialog = Gtk.MessageDialog(window, 0, Gtk.MessageType.QUESTION,
@@ -81,16 +84,12 @@ class Handler:
 		response = confirmDialog.run()
 		if response == Gtk.ResponseType.YES:
 			# Then run the uninstallation script
-			selectedChildIndex=flowbox.get_selected_children()[0] # Only one child is selectable, as set on the .glade structure
+			selectedChildIndex=flowbox.get_selected_children()[0]
 			requestedPath=gamePaths[selectedChildIndex.get_index()]
-			requestedFile=find(requestedPath,"uninstall*")[0] #Assuming there is only one "uninstall"-like file. The "find" function is necessary because there can be different names for this file.
+			requestedFile=find(requestedPath,"uninstall*")[0]
 			subprocess.Popen([requestedFile])
-
 		confirmDialog.destroy()
 
-	def on_open_gog_click(self, *args):
-		subprocess.Popen(["exo-open", "https://www.gog.com"])
-	
 	def on_install_click(self, *args):
 		loadDialog = Gtk.FileChooserDialog("Select the installer :", window,
 		Gtk.FileChooserAction.OPEN,
@@ -110,14 +109,63 @@ class Handler:
 				errorDialog.destroy()
 		loadDialog.destroy()
 
+	def on_new_shortcut_click(self, *args):
+		newShortcutWindow.show()
+	
+	def on_runner_combo_changed(self, runnersCombo):
+		NativeLinuxBox.hide()
+		Wine_Frame.hide()
+		DolphinBox.hide()
+		if runnersCombo.get_active_text()=="Linux Natif":
+			NativeLinuxBox.show()
+		if runnersCombo.get_active_text()=="Wine":
+			Wine_Frame.show()
+		if runnersCombo.get_active_text()=="Dolphin":
+			DolphinBox.show()
+	
+	def on_new_shortcut_done_click(self,button):
+		if runnersCombo.get_active_text()=="Linux Natif":
+			NativeLinuxBox.show()
+		if runnersCombo.get_active_text()=="Wine":
+			Wine_Frame.show()
+		if runnersCombo.get_active_text()=="Dolphin":
+			DolphinBox.show()
+		self.onDeletenewShortcutWindow()
+
+	def WINEDIR_Entry_folder_select(self, entry, icon, eventButton):
+		print("WINEDIR Entry selected")
+		#return(folder)
+
+	def WINEPREFIX_Entry_folder_select(self, entry, icon, eventButton):
+		print("WINEPREFIX Entry selected")
+		#return(folder)
+
 builder=Gtk.Builder()
 builder.add_from_file("layout.glade")
 builder.connect_signals(Handler())
 
+# Main window
 window=builder.get_object("window")
 flowbox=builder.get_object("flowbox")
-description=builder.get_object("description")
-create_lib(flowbox)
+GALLIUM_cpu=builder.get_object("GALLIUM_cpu")
+GALLIUM_fps=builder.get_object("GALLIUM_fps")
+WINEDEBUG_Entry=builder.get_object("WINEDEBUG_Entry")
+disable_compositor=builder.get_object("disable_compositor")
+
+# Installation Window
+nameEntry=builder.get_object("nameEntry")
+newShortcutWindow=builder.get_object("newShortcutWindow")
+newShortcutBox=builder.get_object("newShortcutBox")
+runnersCombo=builder.get_object("runnersCombo")
+Wine_Frame=builder.get_object("Wine_Frame")
+NativeLinuxBox=builder.get_object("NativeLinuxBox")
+NativeLinuxInstallScriptChooser=builder.get_object("NativeLinuxInstallScriptChooser")
+DolphinBox=builder.get_object("DolphinBox")
+DolphinIsoChooser=builder.get_object("DolphinIsoChooser")
+iconFileChooser=builder.get_object("iconFileChooser")
+
+create_lib(flowbox,os.environ["HOME"]+"/GOG Games/")
+create_lib(flowbox,os.environ["HOME"]+"/.games/")
 
 window.show_all()
 Gtk.main()
